c31ee33792df1a61459c5131d1d809debed9eac4
Your Name <you@example.com>
2019-07-01 09:32:09 +0530

    Script Updated

--- Nested Templates/Creation of Two Vnets and peering/Bash.sh
+++ Nested Templates/Creation of Two Vnets and peering/Bash.sh
@@ -6,19 +6,23 @@ $port1=3307
 $rulename1="DisasterRecovery1"
 $rulename2="DisasterRecovery2"
 
-$nsgname="DATABASE" #Change the Name according to need
+$nsgname1="DATABASE1" #Change the Name according to need
+$nsgname1="DATABASE2" #Change the Name according to need
+
+
 
 $resource = Get-AzResource | Where {$_.ResourceGroupName –eq $RG -and $_.ResourceType -eq "Microsoft.Network/networkSecurityGroups"} 
-$nsg = Get-AzNetworkSecurityGroup -Name $nsgname -ResourceGroupName $RG
+$nsg1 = Get-AzNetworkSecurityGroup -Name $nsgname1 -ResourceGroupName $RG
+$nsg2 = Get-AzNetworkSecurityGroup -Name $nsgname2 -ResourceGroupName $RG
 
 # Add the inbound security rule.
-$nsg | Add-AzNetworkSecurityRuleConfig -Name $rulename1 -Description "AllowCommFromD2toD1" -Access Allow `
-    -Protocol * -Direction Inbound -Priority 3891 -SourceAddressPrefix "15.0.2.0/24" -SourcePortRange $port1 `
-    -DestinationAddressPrefix "10.0.2.0/24" -DestinationPortRange $port1
+$nsg2 | Add-AzNetworkSecurityRuleConfig -Name $rulename1 -Description "AllowCommFromD2toD1" -Access Allow `
+    -Protocol * -Direction Inbound -Priority 3891 -SourceAddressPrefix "10.0.2.0/24" -SourcePortRange $port1 `
+    -DestinationAddressPrefix "15.0.2.0/24" -DestinationPortRange $port1
 
 
 # Add the outbound security rule.
-$nsg | Add-AzNetworkSecurityRuleConfig -Name $rulename2 -Description "AllowCommFromD2toD1" -Access Allow `
+$nsg1 | Add-AzNetworkSecurityRuleConfig -Name $rulename2 -Description "AllowCommFromD2toD1" -Access Allow `
     -Protocol * -Direction Outbound -Priority 3891 -SourceAddressPrefix "10.0.2.0/24" -SourcePortRange $port1 `
     -DestinationAddressPrefix "15.0.2.0/24" -DestinationPortRange $port1
 
