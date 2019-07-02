06b069a431a98b874574d7e5bc1cb10903072ba8
Your Name <you@example.com>
2019-07-02 20:34:09 +0530

    .tf file updated

--- Nested Templates/Creation of Two Vnets and peering/TEMPLATE1.tf
+++ Nested Templates/Creation of Two Vnets and peering/TEMPLATE1.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_template_deployment" "test" {
-  name                = "Main-Deploy"
+  name                = "Main-Deploy-1"
   resource_group_name = "${azurerm_resource_group.test.name}"
 
   template_body = "${file("./main.json")}"
@@ -41,7 +41,6 @@ resource "azurerm_template_deployment" "test" {
       "secondNetworkSecurityGroupInSecondLocaion"="A2",
       "thirdNetworkSecurityGroupInSecondLocaion"="D2",
       "enableDdosProtection"=null,
-      "virtualNetworkAccess"=true,
       "forwardedTraffic"=null,
       "gatewayTransit"=null,
       "remoteGateways"=null
