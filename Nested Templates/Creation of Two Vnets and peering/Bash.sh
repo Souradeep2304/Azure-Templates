5d8e9ac838fc6c7d8d73dcc6804fc9c644d95ec0
Your Name <you@example.com>
2019-07-18 14:20:33 +0530

    script updated

--- Nested Templates/Creation of Two Vnets and peering/Bash.sh
+++ Nested Templates/Creation of Two Vnets and peering/Bash.sh
@@ -1,8 +1,10 @@
 $RG="MyResourceGrp"
 $port1=3307
 
-$rulename="DisasterRecovery1"
-
+$R1="DR1"
+$R2="DR2"
+$R3="DR3"
+$R4="DR4"
 
 $N1="D1" #Change the Name according to need
 $N2="D2" #Change the Name according to need
@@ -14,24 +16,24 @@ $nsg1 = Get-AzNetworkSecurityGroup -Name $N1 -ResourceGroupName $RG
 $nsg2 = Get-AzNetworkSecurityGroup -Name $N2 -ResourceGroupName $RG
 
 # Add the inbound security rule.
-$nsg2 | Add-AzNetworkSecurityRuleConfig -Name $rulename -Description "AllowCommFromD1toD2" -Access Allow `
+$nsg2 | Add-AzNetworkSecurityRuleConfig -Name $R1 -Description "AllowCommFromD1toD2" -Access Allow `
     -Protocol * -Direction Inbound -Priority 3891 -SourceAddressPrefix "10.0.2.0/24" -SourcePortRange $port1 `
     -DestinationAddressPrefix "15.0.2.0/24" -DestinationPortRange $port1
 
 
 # Add the outbound security rule.
-$nsg1 | Add-AzNetworkSecurityRuleConfig -Name $rulename -Description "AllowCommFromD1toD2" -Access Allow `
+$nsg1 | Add-AzNetworkSecurityRuleConfig -Name $R2 -Description "AllowCommFromD1toD2" -Access Allow `
     -Protocol * -Direction Outbound -Priority 3891 -SourceAddressPrefix "10.0.2.0/24" -SourcePortRange $port1 `
     -DestinationAddressPrefix "15.0.2.0/24" -DestinationPortRange $port1
 
 # Add the inbound security rule.
-$nsg1 | Add-AzNetworkSecurityRuleConfig -Name $rulename -Description "AllowCommFromD2toD1" -Access Allow `
+$nsg1 | Add-AzNetworkSecurityRuleConfig -Name $R3 -Description "AllowCommFromD2toD1" -Access Allow `
     -Protocol * -Direction Inbound -Priority 3891 -SourceAddressPrefix "15.0.2.0/24" -SourcePortRange $port1 `
     -DestinationAddressPrefix "10.0.2.0/24" -DestinationPortRange $port1
 
 
 # Add the outbound security rule.
-$nsg2 | Add-AzNetworkSecurityRuleConfig -Name $rulename -Description "AllowCommFromD2toD1" -Access Allow `
+$nsg2 | Add-AzNetworkSecurityRuleConfig -Name $R4 -Description "AllowCommFromD2toD1" -Access Allow `
     -Protocol * -Direction Outbound -Priority 3891 -SourceAddressPrefix "15.0.2.0/24" -SourcePortRange $port1 `
     -DestinationAddressPrefix "10.0.2.0/24" -DestinationPortRange $port1
 
